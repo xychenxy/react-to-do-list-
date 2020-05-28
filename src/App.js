8ce68001c3da338a6bb4e43e5ba6754f92138061
xychenxy <mavischen916@gmail.com>
2020-05-29 00:07:50 +1000

    setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
-import TodoItem from "./components/TodoItem";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import "bootstrap/dist/css/bootstrap.min.css"
+import uuid from 'uuid';
 
 export default class App extends Component{
     render(){
         return(
-            <section>
-              <h2>Todo List</h2>
-              <TodoItem/>
-            </section>
+            <div>
+                <div className="container">
+                    <div className="row">
+                        <TodoInput/>
+                        <TodoList/>
+                        <div className="col-4 bg-warning">1</div>
+                        <div className="col-4 bg-danger">2</div>
+                        <div className="col-4 bg-warning">3</div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
